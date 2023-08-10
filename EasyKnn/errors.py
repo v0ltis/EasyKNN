d459f6df4011379d136ebc6f974f63482b62ccc7
v0ltis <voltis.discord@gmail.com>
2023-08-11 01:04:06 +0200

    Added errors.py

---
+++ EasyKnn/errors.py
@@ -0,0 +1,23 @@
+class ValueAlreadyLinkedError(Exception):
+    """Raised when trying to add a single value in two different datasets"""
+    pass
+
+
+class DatasetAlreadyLinkedError(Exception):
+    """Raised when trying to add a dataset to two different plans"""
+    pass
+
+
+class NoDimensionError(Exception):
+    """Raised when trying to add a value with no dimensions to a dataset"""
+    pass
+
+
+class ReadOnlyAttributeError(Exception):
+    """Raised when trying to modify a read-only attribute"""
+    pass
+
+
+class CriticalValueDeletionError(Exception):
+    """Raised when trying to delete a critical value"""
+    pass
