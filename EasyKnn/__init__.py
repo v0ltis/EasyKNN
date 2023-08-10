fb6868e1168af2e81e3c7a1c714e0a73f6441656
v0ltis <voltis.discord@gmail.com>
2023-08-11 01:21:25 +0200

    Imported errors in __init__.py

--- EasyKnn/__init__.py
+++ EasyKnn/__init__.py
@@ -3,3 +3,5 @@ from EasyKnn.dataset import Dataset
 from EasyKnn.plan import Plan
 from EasyKnn.neighbours import Neighbours
 from EasyKnn.point import Point
+from EasyKnn.errors import ValueAlreadyLinkedError, DatasetAlreadyLinkedError, \
+                           NoDimensionError, ReadOnlyAttributeError, CriticalValueDeletionError
